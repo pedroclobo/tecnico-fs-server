9b35c38b4bc322b78fc0cbb76b55c0eecf9eb048
Pedro Lobo <pedrocerqueiralobo@gmail.com>
2022-01-25 20:57:10 +0000

    Start requisite 2

--- client/tecnicofs_client_api.c
+++ client/tecnicofs_client_api.c
@@ -1,8 +1,57 @@
 #include "tecnicofs_client_api.h"
+#include <fcntl.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 int tfs_mount(char const *client_pipe_path, char const *server_pipe_path) {
-	/* TODO: Implement this */
-	return -1;
+    if (unlink(client_pipe_path) != 0) {
+		return -1;
+    }
+    if (mkfifo(client_pipe_path, 0640) != 0) {
+        return -1;
+    }
+
+	int client_pipe;
+	int server_pipe;
+	if ((client_pipe = open(client_pipe_path, O_WRONLY)) != 0) {
+		return -1;
+	}
+	if ((server_pipe = open(client_pipe_path, O_RDONLY)) != 0) {
+		return -1;
+	}
+
+	char str[43] = { TFS_OP_CODE_MOUNT, '|', '\0' };
+	strcat(str, client_pipe_path);
+
+	ssize_t ret;
+
+    size_t len = strlen(str);
+    size_t written = 0;
+    while (written < len) {
+        ret = write(server_pipe, str + written, len - written);
+        if (ret < 0) {
+			return -1;
+        }
+        written += ret;
+    }
+
+	ssize_t received;
+	char session_id[100];
+    while (received != 0) {
+        ret = read(server_pipe, session_id + received, 1);
+        if (ret < 0) {
+			return -1;
+        }
+        written += ret;
+    }
+	if (ret < 0) {
+		return -1;
+	}
+
+	return 0;
 }
 
 int tfs_unmount() {
--- fs/tfs_server.c
+++ fs/tfs_server.c
@@ -1,4 +1,10 @@
 #include "operations.h"
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define S 100
 
 int main(int argc, char **argv) {
 
@@ -6,12 +12,24 @@ int main(int argc, char **argv) {
 		printf("Please specify the pathname of the server's pipe.\n");
 		return 1;
 	}
-
 	char *pipename = argv[1];
-
 	printf("Starting TecnicoFS server with pipe called %s\n", pipename);
 
-	/* TO DO */
+	/* Unlink and create server pipe */
+	unlink(pipename);
+	if (mkfifo(pipename, 0777) < 0) {
+		exit(-1);
+	}
+
+	/* Open server pipe */
+	int fserver;
+	if ((fserver = open(pipename, O_RDONLY)) != 0) {
+		exit(-1);
+	};
+
+	/* Close and unlink server pipe */
+	close(fserver);
+	unlink(pipename);
 
 	return 0;
 }
