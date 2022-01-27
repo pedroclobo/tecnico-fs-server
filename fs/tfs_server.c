9345e7b20de056d42eadab458ce8a33327315e0d
dredegui <gui.pascoal@outlook.com>
2022-01-27 22:41:59 +0000

    V.Produto/Consumidor

--- fs/tfs_server.c
+++ fs/tfs_server.c
@@ -8,33 +8,41 @@
 
 #define S 1
 #define PIPE_NAME_MAX_SIZE 41
-
+#define BUFFER_SIZE 1000
 enum state{CREATED = 2, TO_CREATE = 1, FREE = 0};
 
+
 typedef struct {
+	int opcode;
+	int session_id;
 	int fhandle;
+	int flags;
 	char *str;
 	size_t size;
-} wr_struct;
-
+} input_struct;
 
 typedef struct {
 	pthread_t thread;
-	enum state curr;
+	pthread_mutex_t lock_buffer;
+	pthread_cond_t a;
+	pthread_cond_t b;
+	int count;
+	char buffer[BUFFER_SIZE];
+	bool free;
 	int pipe;
 } session;
 
 session sessions[S];
 
-void *write_to_tfs(void *w_struct) {
-	wr_struct *w = (wr_struct*) w_struct;
-	tfs_write(w->fhandle, w->str, strlen(w->str));
-	return NULL;
+void slave_thread(void* arg) {
+	int *session_id = (int*)arg;
+	/*resto*/
+
 }
 
 void init_server() {
 	for (int s = 0; s < S; s++) {
-		sessions[s].curr = FREE;
+		sessions[s].free = true;
 	}
 }
 
@@ -68,17 +76,16 @@ int main(int argc, char **argv) {
 	while (1) {
 
 		/* Read op-code */
-		char opcode;
-		if (read(server_pipe, &opcode, sizeof(char)) == -1) {
+		input_struct input;
+		if (read(server_pipe, &input, sizeof(input_struct)) == -1) {
 			return -1;
 		}
-
-		if (opcode == TFS_OP_CODE_MOUNT) {
+		if (input.opcode == TFS_OP_CODE_MOUNT) {
 
 			/* Compute new session_id */
 			int session_id = -1;
 			for (int s = 0; s < S; s++) {
-				if (sessions[s].curr == FREE) {
+				if (sessions[s].free == true) {
 					session_id = s;
 				}
 			}
@@ -89,7 +96,7 @@ int main(int argc, char **argv) {
 			}
 
 			/* Thread is now occupied */
-			sessions[session_id].curr = TO_CREATE;
+			sessions[session_id].free = false;
 
 			/* Read client pipe path from client */
 			char client_pipe_path[PIPE_NAME_MAX_SIZE];
@@ -109,37 +116,41 @@ int main(int argc, char **argv) {
 				return -1;
 			}
 
-		} else if (opcode == TFS_OP_CODE_UNMOUNT) {
+		} else if (input.opcode == TFS_OP_CODE_UNMOUNT) {
 			int session_id;
 			if (read(server_pipe, &session_id, sizeof(int)) == -1) {
 				return -1;
 			}
-			sessions[session_id].curr = FREE;
-		}
-		else {
-			int session_id;
-			if (read(server_pipe, &session_id, sizeof(int)) == -1) {
-				return -1;
+			sessions[session_id].free = true;
+		} else {
+			int sent_bytes = 0;
+			session curr_session = sessions[input.session_id];
+			while(sent_bytes < sizeof(input) + input.size || (sent_bytes < sizeof(input) + input.size  && input.opcode == TFS_OP_CODE_OPEN)) {
+				pthread_mutex_lock(&curr_session.lock_buffer);
+				while (curr_session.count == BUFFER_SIZE) pthread_cond_wait(&curr_session.a, &curr_session.lock_buffer);
+				memcpy(curr_session.buffer, input.opcode, sizeof(int));
+				if (input.fhandle != -1) {
+					if (sizeof(int) + curr_session.count >= BUFFER_SIZE) pthread_cond_wait(&curr_session.a, &curr_session.lock_buffer);
+					memcpy(curr_session.buffer, input.fhandle, sizeof(int));
+					curr_session.count += sizeof(int); 
+				}
+				if (input.flags != -1) {
+					if (sizeof(int) + curr_session.count >= BUFFER_SIZE) pthread_cond_wait(&curr_session.a, &curr_session.lock_buffer);
+					memcpy(curr_session.buffer, input.fhandle, sizeof(int));
+					curr_session.count += sizeof(int);
+				}
+				if (input.size != -1) {
+					if (sizeof(size_t) + curr_session.count >= BUFFER_SIZE) pthread_cond_wait(&curr_session.a, &curr_session.lock_buffer);
+					memcpy(curr_session.buffer, input.size, sizeof(size_t));
+					curr_session.count += sizeof(size_t);
+					if (input.size + curr_session.count >= BUFFER_SIZE) pthread_cond_wait(&curr_session.a, &curr_session.lock_buffer);
+					memcpy(curr_session.buffer, input.str, input.size);
+					curr_session.count += input.size;
+				}
+				pthread_cond_signal(&curr_session.b);
 			}
-			if (opcode == TFS_OP_CODE_WRITE) {
-				int f;
-				size_t size;
-				if (read(server_pipe, &f, sizeof(int)) == -1) {
-					return -1;
-				}
-				if (read(server_pipe, &size, sizeof(size_t)) == -1) {
-					return -1;
-				}
-				char str[size];
-				if (read(server_pipe, str, size) == -1) {
-					return -1;
-				}
-				wr_struct w = { f, str, size};
-				if (sessions[session_id].curr == TO_CREATE) {
-					pthread_create(sessions[session_id].thread, NULL, write_to_tfs, (void*)&w);
-				}
-			}			
 		}
+
 	}
 
 	/* Close and unlink server pipe */
